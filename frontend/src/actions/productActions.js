4b9559c6d6bbb30461ed5dbd7c3df301d159d390
Ernesto Garozzo <ernesto.garozzo@avanade.com>
2023-01-10 21:43:48 +0100

    discard node modules

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,7 +15,7 @@ import {
 export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
-        const { data } = await axios.get(`/api/products`)
+        const { data } = await axios.get(`http://127.0.0.1:8000/api/products`)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
@@ -35,7 +35,7 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST })
-        const { data } = await axios.get(`/api/products/${id}`)
+        const { data } = await axios.get(`http://127.0.0.1:8000/api/products/${id}`)
 
         dispatch({
             type: PRODUCT_DETAILS_SUCCESS,
